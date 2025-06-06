a2dc1ae07dca163df8fd1364a50c2309fb36a585
Charlie <112635823+charliepain@users.noreply.github.com>
2025-06-06 17:35:36 -0400

    Implement book adding feature

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const books = []
+const library = []
 
 function Book(title, author, numberPages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
+
+    this.id = crypto.randomUUID();
+
     this.title = title;
     this.author = author;
     this.numberPages = numberPages;
@@ -14,3 +17,147 @@ function Book(title, author, numberPages, read) {
         console.log(`${this.title} by ${this.author}, ${numberPages} pages, ${readingStatus}`);
     }
 }
+
+function addBookToLibrary(title, author, numberPages, read) {
+    let book = new Book(title,author,numberPages,read); 
+    library.push(book);
+}
+
+function displayBooksCard() {
+    const body = document.querySelector("body");
+
+    // Destroy old book section
+    const oldBookSection = document.querySelector(".book-section");
+    if (oldBookSection != null) {
+        oldBookSection.remove();
+    }
+
+    // Display new book section
+
+    const bookSection = document.createElement("div");
+    bookSection.classList.add("book-section");
+    body.appendChild(bookSection);
+
+    if (library.length <= 0) {
+        bookSection.textContent = "There are no books in your library.";
+        return;
+    }
+
+    for (let book of library) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookSection.appendChild(bookCard);
+        
+        const id = document.createElement("div");
+        const title = document.createElement("div");
+        const author = document.createElement("div");
+        const numberPages = document.createElement("div");
+        const read = document.createElement("div");
+        
+        id.textContent = `Id: ${book["id"]}`;
+        title.textContent = `Title: ${book["title"]}`;
+        author.textContent = `Author: ${book["author"]}`;
+        numberPages.textContent = `Number of pages: ${book["numberPages"]}`;
+        read.textContent = `Read: ${book["read"] ? "Yes" : "No"}`;
+
+        bookCard.appendChild(id);
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(numberPages);
+        bookCard.appendChild(read);
+    }
+}
+
+function displayBookForm() {
+
+    const form = document.createElement("form");
+    form.classList.add("book-form");
+    
+    const titleInput = document.createElement("input");
+    const authorInput = document.createElement("input");
+    const numberPagesInput = document.createElement("input");
+    const readInput = document.createElement("input");
+
+    titleInput.setAttribute("id","title");
+    titleInput.setAttribute("name","title");
+    titleInput.setAttribute("type","text");
+
+    authorInput.setAttribute("id","author");
+    authorInput.setAttribute("name","author");
+    authorInput.setAttribute("type","text");
+    
+    numberPagesInput.setAttribute("id","number-pages");
+    numberPagesInput.setAttribute("name","number-pages");
+    numberPagesInput.setAttribute("type","number");
+    numberPagesInput.setAttribute("step","1");
+
+    readInput.setAttribute("id","read");
+    readInput.setAttribute("name","read");
+    readInput.setAttribute("type","checkbox");
+
+    const titleLabel = document.createElement("label");
+    const authorLabel = document.createElement("label");
+    const numberPagesLabel = document.createElement("label");
+    const readLabel = document.createElement("label");
+
+    titleLabel.setAttribute("for","title");
+    authorLabel.setAttribute("for","author");
+    numberPagesLabel.setAttribute("for","number-pages");
+    readLabel.setAttribute("for","read");
+
+    titleLabel.textContent = "Title";
+    authorLabel.textContent = "Author";
+    numberPagesLabel.textContent = "Number of pages";
+    readLabel.textContent = "I have read this book";
+
+    const newBookButton = document.querySelector(".new-book-container");
+    newBookButton.after(form);
+
+    const addBookButton = document.createElement("button");
+    addBookButton.setAttribute("type","submit");
+    addBookButton.textContent = "Add this book";
+    addBookButton.addEventListener("click",
+        e => {
+            const title = titleInput.value;
+            const author = authorInput.value;
+            const numberPages = numberPagesInput.value;
+            const read = readInput.checked;
+             
+            addBookToLibrary(title,author,numberPages,read);
+            displayBooksCard();
+            e.preventDefault();
+        }
+    );
+
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+    form.appendChild(authorLabel);
+    form.appendChild(authorInput);
+    form.appendChild(numberPagesLabel);
+    form.appendChild(numberPagesInput);
+    form.appendChild(readLabel);
+    form.appendChild(readInput);
+    form.appendChild(addBookButton);
+}
+
+
+document.querySelector(".new-book").addEventListener("click",
+    () => {
+
+        const oldBookForm = document.querySelector(".book-form");
+        if (oldBookForm == null) {
+            displayBookForm();
+        }
+        else {
+            oldBookForm.remove();
+        }
+
+    });
+
+// addBookToLibrary("A","Ason",200,true);
+// addBookToLibrary("B","Bson",300,false);
+// addBookToLibrary("C","Cson",400,true);
+// addBookToLibrary("D","Dson",500,false);
+
+displayBooksCard();
+
